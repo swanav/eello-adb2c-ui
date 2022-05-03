959c281c61780a9a457f537b0403d6d9250c6eae
Swanav <swanav@outlook.com>
2022-05-03 14:18:15 +0530

    Updated socialsignin

--- main.js
+++ main.js
@@ -40,6 +40,65 @@ function styleSocialSignInPage() {
         const facebookAccountButton = unifiedSelectorDiv.querySelector('.accountButton#FacebookExchange')
         modifyActionButton(facebookAccountButton, 'fab fa-facebook-f', 'Facebook')
     }
+
+    const localSignInDiv = document.querySelector('#localAccountForm.localAccount')
+    console.log(localSignInDiv)
+    if (localSignInDiv) {
+        localSignInDiv.classList.add('card', 'p-5')
+        localSignInDiv.querySelector('.intro').remove();
+        const form = localSignInDiv
+        form.classList.add('is-flex', 'is-flex-direction-column')
+        form.childNodes.forEach((e) => {
+            if (e.nodeType === Node.ELEMENT_NODE) {
+                e.classList.add('is-flex-grow-1', 'm-2', 'p-2')
+            }
+        })
+
+        const errorDivs = localSignInDiv.querySelectorAll('.error')
+        errorDivs.forEach(errorDiv => {
+            styleErrorElement(errorDiv)
+        })
+
+        form.querySelector('label[for=signInName]').classList.add('is-hidden')
+        form.querySelector('#signInName').classList.add('input', 'my-2')
+        form.querySelector('label[for=password]').classList.add('is-hidden')
+        form.querySelector('#forgotPassword').classList.add('is-pulled-right', 'button', 'is-inverted', 'is-rounded', 'is-small', 'is-danger', 'my-4')
+        form.querySelector('#password').classList.add('input', 'my-2')
+        form.querySelector('.rememberMe').classList.add('my-4')
+        form.querySelector('#rememberMe').classList.add('mr-2')
+        form.querySelector('label[for=rememberMe]').classList.add('mx-2', 'has-text-danger')
+
+        form.querySelectorAll('button').forEach(e => {
+            e.classList.add('button', 'is-flex-grow-1', 'is-primary', 'my-5')
+        })
+
+        form.querySelector('.divider').classList.add('has-text-centered');
+
+        const createAccountSelector = form.querySelector('.create')
+        if (createAccountSelector) {
+            const createAccountPrompt = createAccountSelector.children[0]
+            if (createAccountPrompt) {
+                let layoutClass = document.body.clientWidth > 640 ? 'is-flex-direction-row' : 'is-flex-direction-column'
+                createAccountPrompt.classList.add('is-flex', layoutClass, 'is-justify-content-center')
+
+                const signUpLink = createAccountPrompt.childNodes[1]
+
+                const promptDiv = document.createElement('div')
+                promptDiv.classList.add('mx-2', 'has-text-centered')
+                promptDiv.innerText = "Don't have an account?"
+
+
+                createAccountPrompt.innerHTML = ''
+                createAccountPrompt.appendChild(promptDiv)
+                if (signUpLink) {
+                    createAccountPrompt.appendChild(signUpLink)
+                    signUpLink.classList.add('has-text-centered', 'has-text-primary')
+                }
+            }
+    
+    
+        }
+    }
 }
 
 function styleErrorElement(element) {
